1c2d499deac0301a04360fff8623f8aeab358138
mäcci <fabbycrafted@gmail.com>
2024-10-08 20:35:04 +0300

    neovim

--- users/mitchellh/neovim.nix
+++ users/mitchellh/neovim.nix
@@ -5,5 +5,5 @@
 
 	#Use external directory for neovim
 	home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink
-		"${config.home.homeDirectory}/nixos-arm-config/nvim"
+		"${config.home.homeDirectory}/nixos-arm-config/nvim";
 }
